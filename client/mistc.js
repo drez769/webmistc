8c51a6d0de21be132fbcfe53daacb2d5dddf8a97
drez769 <drez769@gmail.com>
2016-12-10 13:52:25 -0500

    Playback slider, syncing, and other upgrades
    Implemented the playback slider, fixed playback syncing, show closest
    last slide on playback (sorta), allow multiple zip uploads and correctly
    play back

--- client/mistc.js
+++ client/mistc.js
@@ -277,7 +277,41 @@ if (Meteor.isClient) {
 
     // Control
     Template.controlPanel.onRendered(function () {
-        $('.slider-selection').slider({
+		// Event handler for moving/clicking the playback slider.
+		let setPlayback = function() {
+			let sliderValue = $('#pbslider').data('slider').getValue();
+			let video = document.getElementById('uploadedRecording');			
+			let sliderMax = $("#pbslider").data("slider-max");
+			let percent = sliderValue / sliderMax;
+            video.currentTime = percent * video.duration;
+			
+			Playback.pause();
+			Playback.updateSlider();
+			if (video.paused) return;
+			Playback.play();
+		};
+		
+		// Playback slider set up and event handlers
+        $('#pbslider').slider({
+            formatter: function (value) {
+                return 'Current value: ' + value;
+            }
+        }).on('slide', setPlayback) // slide event
+		// .on('slideStop', setPlayback)
+		.data('slider');
+		
+		// 'hack' to handle a single click event on the slider
+		$('.slider-horizontal').mousedown(setPlayback);
+
+		// Volume and mic sliders. Doesn't seem possible to easily control
+		// volume or mic from web browser at this time.
+		$('#volumeSlider').slider({
+            formatter: function (value) {
+                return 'Current value: ' + value;
+            }
+        });
+		
+		$('#micSlider').slider({
             formatter: function (value) {
                 return 'Current value: ' + value;
             }
@@ -298,25 +332,28 @@ if (Meteor.isClient) {
             jsZip.loadAsync(jqRecording).then(function (zip) {
                 zip.file("recording.json").async("string").then(function (recordedJsonStr) {
                     zip.file("recording.webm").async("uint8array").then(function (videoData) {
+						$('#uploadedRecording').remove();
                         let video = document.createElement('video');
                         video.src = URL.createObjectURL(new Blob([videoData], {type: 'video/webm'}));
                         video.controls = true;
                         //remove the video once the recording has stopped playing
                         video.onended = function () {
                             // document.getElementById('control-fluid').removeChild(video);
-                            Playback.cleanup();
-							$('#pbslider').slider('setValue', $("#pbslider").data("slider-max"));
+							$('.btn-play')[0].disabled = false;
+							Playback.stop();
+							Playback.updateSlider();
+							// $('#pbslider').slider('setValue', $("#pbslider").data("slider-max"));
                         };
-                        //add the video and play the JSON / video simultaneously.
                         document.getElementById('control-fluid').appendChild(video);
-						
 						video.style.display = 'none'; //hide the video player (no need to see it)
-						//TODO make sure old video players are removed when a new video is uploaded?
                         video.id = 'uploadedRecording';
+						Playback.stop();
                         Playback.upload(JSON.parse(recordedJsonStr));
 						
-						// Start playback once the video is loaded
+						// Start JSON / video playback simultaneously once the video is loaded
 						video.onloadeddata = function () {
+							$('.btn-play')[0].disabled = true;
+							$('.btn-pause')[0].disabled = false;
 							video.play();
 							Playback.play();
 						};
@@ -331,17 +368,23 @@ if (Meteor.isClient) {
 
         },
         'click .btn-stop': function () {
+			$('.btn-play')[0].disabled = false;
+			$('.btn-pause')[0].disabled = true;
             let video = document.getElementById('uploadedRecording');
             video.currentTime = 0;
             video.pause();
             Playback.stop();
         },
         'click .btn-pause': function () {
+			$('.btn-play')[0].disabled = false;
+			$('.btn-pause')[0].disabled = true;
             let video = document.getElementById('uploadedRecording');
             video.pause();
             Playback.pause();
         },
         'click .btn-play': function () {
+			$('.btn-play')[0].disabled = true;
+			$('.btn-pause')[0].disabled = false;
             let video = document.getElementById('uploadedRecording');
             video.play();
             Playback.play();
--- imports/playback-library.js
+++ imports/playback-library.js
@@ -11,6 +11,10 @@ let videoTracking = null;
 let pbSlideTracker = null; //Timer to keep the playback slider in the correct position
 let startedTime = -1;
 let elapsedTime = -1;
+let totalTime = 0; //Unused. Total time a recording has been played back so far.
+				   //Would be needed to fix syncing bug that previously existed.
+				   //Instead, syncing is done based on an audio/video recording timer.
+				   //A few other code changes would be needed to use this correctly...
 let recordings = "";
 let isPaused = false;
 
@@ -50,12 +54,15 @@ Playback.skipBack = function () {
     if (isPaused) {
         elapsedTime = elapsedTime - 5000;
         if (elapsedTime < 0) {elapsedTime = 0;}
+		totalTime = totalTime > 5000 ? totalTime-5000 : 0;
     } else {
         Playback.pause();
         elapsedTime = elapsedTime - 5000;
         if (elapsedTime < 0) {elapsedTime = 0;}
+		totalTime = totalTime > 5000 ? totalTime-5000 : 0;
         Playback.play();
     }
+	Playback.updateSlider();
 };
 
 Playback.stop = function () {
@@ -66,9 +73,11 @@ Playback.stop = function () {
     _.each(timings, function(timing){
         Meteor.clearTimeout(timing)
     })
-    isPaused = false;
+	timings = [];
+    isPaused = true;
     startedTime = -1;
     elapsedTime = -1;
+	totalTime = 0;
 };
 
 Playback.pause = function () {
@@ -77,8 +86,10 @@ Playback.pause = function () {
     _.each(timings, function(timing){
         Meteor.clearTimeout(timing)
     })
+	timings = [];
     isPaused = true;
     elapsedTime = Date.now()-startedTime;
+	totalTime += elapsedTime;
 };
 
 Playback.play = function () {
@@ -86,47 +97,77 @@ Playback.play = function () {
     let firstTimeStamp = self.start;
     isPaused = false;
     startedTime = Date.now();
+	let video = document.getElementById('uploadedRecording');
 	
 	// If playback is NOT being started from a stop
     if (elapsedTime != -1) {
-        firstTimeStamp += elapsedTime;
-        startedTime += elapsedTime;
+		firstTimeStamp += (video.currentTime*1000);
+		
+		// Using totalTime here fixes the previous bug, but syncing with video is better
+        // firstTimeStamp += totalTime;
         elapsedTime = -1;
     }
-    _.each(recordings, function (recording) {
-        if (recording.time - firstTimeStamp > 0) {
+	
+	// For each recorded JSON event, check if it should be queued to play
+	// based on the amount of time played so far
+	let foundNextUpdate = false;
+    _.each(recordings, function (recording, i) {
+		
+		// If a recorded JSON event should come after where we currently are in time
+        if (recording.time >= firstTimeStamp) {
+			// Ideally this should update the slideshow to show the closest last slide,
+			// however probably won't work if the last JSON event was a marker or something
+			//TODO better to implement a function to find the last slide change to show instead
+			if (!foundNextUpdate) {
+				foundNextUpdate = true;
+				let lastUpdate = i > 0 ? i-1 : 0;
+				updateSlideShow(recordings[lastUpdate]);
+			}
+			
+			// This JSON event comes after current playback time, so queue it to play back
             timings.push(Meteor.setTimeout(
-                function () {
-                    switch (recording.state) {
-                        case 'session':
-                            const sessionState = recording.params[0];
-                            Session.set(recording.action, sessionState);
-                            break;
-                        case 'database':
-                            const isReplaceOn = Session.get('overlay.tool.replace');
-                            if (isReplaceOn) {
-                                const title = recording.params[0];
-                                const page = recording.params[1];
-                                replaceNote('previous', title, page);
-                            }
-                            Meteor.apply(recording.action, recording.params);
-                            break;
-                    }
-                },
+                function () { updateSlideShow(recording); },
                 ( parseInt(recording.time - firstTimeStamp) )
             ));
         }
     });
 };
 
+// Updates the slideshow to show the next JSON event
+let updateSlideShow = function (recording) {
+	switch (recording.state) {
+	case 'session':
+		const sessionState = recording.params[0];
+		Session.set(recording.action, sessionState);
+		break;
+	case 'database':
+		const isReplaceOn = Session.get('overlay.tool.replace');
+		if (isReplaceOn) {
+			const title = recording.params[0];
+			const page = recording.params[1];
+			replaceNote('previous', title, page);
+		}
+		Meteor.apply(recording.action, recording.params);
+		break;
+	}
+	let video = document.getElementById('uploadedRecording');
+	// console.log("Current vid - this");
+	// console.log(video.currentTime);
+	// console.log(recording.time - self.start + elapsedTime)
+	// console.log(firstTimeStamp);
+}
+
 Playback.skipForward = function () {
     if (isPaused) {
         elapsedTime = elapsedTime + 5000;
+		totalTime += 5000;
     } else {
         Playback.pause();
         elapsedTime = elapsedTime + 5000;
+		totalTime += 5000;
         Playback.play();
     }
+	Playback.updateSlider();
 };
 
 // This is straight from slackOverflow
@@ -142,13 +183,15 @@ String.prototype.toHHMMSS = function () {
     return hours+':'+minutes+':'+seconds;
 };
 
+// Function to use timers to keep track of current playback time and slider position
 trackVideo = function (){
 	let video = document.getElementById('uploadedRecording');
+	document.getElementById('duration').placeholder=video.duration.toString().toHHMMSS();
 
 	// Timer to keep track of the current playback time in the video
     videoTracking = Meteor.setInterval(
 		function(){
-			console.log("currentTime: "+video.currentTime);
+			// console.log("currentTime: "+video.currentTime);
 			document.getElementById('position').placeholder=video.currentTime.toString().toHHMMSS();
 		}, 500 // milliseconds
 	);
@@ -156,17 +199,27 @@ trackVideo = function (){
 	// For tracking the playback slider
 	let sliderMax = $("#pbslider").data("slider-max");
 	let interval = video.duration / sliderMax;
-	if (video.currentTime === 0) $('#pbslider').slider('setValue', 0);
-	if (video.currentTime !== 0) $('#pbslider').slider('setValue', video.currentTime / interval);
+	Playback.updateSlider();
 	pbSlideTracker = Meteor.setInterval(function () {
 		let currentPosition = video.currentTime / interval;
-		console.log("Slide: " + video.currentTime / interval)
+		// console.log("Slide: " + video.currentTime / interval)
+		// console.log("currentTime " + video.currentTime);
+		// console.log(Date.now()-startedTime);
 		$('#pbslider').slider('setValue', currentPosition);
 	}, interval * 1000);
 };
 
+// Update the playback slider based on the current A/V recording time
+Playback.updateSlider = function() {
+	let video = document.getElementById('uploadedRecording');
+	let sliderMax = $("#pbslider").data("slider-max");
+	let interval = video.duration / sliderMax;
+	$('#pbslider').slider('setValue', video.currentTime / interval);
+	document.getElementById('position').placeholder=video.currentTime.toString().toHHMMSS();
+}
+
+// Clear timers that track playback position and slider position
 Playback.cleanup = function() {
-	console.log("playback stopped");
     Meteor.clearInterval(videoTracking);
 	Meteor.clearInterval(pbSlideTracker);
 }
